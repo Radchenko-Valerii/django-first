c187b3fa7bab26e9a8405fc9c02ae6302a53fc2d
Valerii Radchenko <valerii.radchenko@freshcodeit.com>
2022-02-07 16:06:35 +0200

    update urls & views

--- mainapp/views.py
+++ mainapp/views.py
@@ -1,10 +1,25 @@
 from django.shortcuts import render
-from django.http import  HttpResponse
+from django.http import HttpResponse
 from .models import Task
+from .forms import TaskForm
+
 
 def index(req):
     tasks = Task.objects.all()
     return render(req, 'mainapp/index.html', {'title': 'main page of site', 'tasks': tasks})
 
+
 def about(req):
     return render(req, 'mainapp/about.html')
+
+def create(req):
+    if req.method == 'POST':
+        form = TaskForm(req.POST)
+        if form.is_valid():
+            form.save()
+
+    form = TaskForm()
+    context = {
+        'form': form
+    }
+    return render(req, 'mainapp/create.html', context)
