f1872908f899a467c279af3bdded7900a477afad
Valerii Radchenko <valerii.radchenko@freshcodeit.com>
2022-02-07 14:53:41 +0200

    add base logic for app

--- mainapp/models.py
+++ mainapp/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+
+class Task(models.Model):
+    title = models.CharField('name', max_length=56)
+    task = models.TextField('lol')
+
+    def __str__(self):
+        return self.title
--- mainapp/views.py
+++ mainapp/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.http import  HttpResponse
+from .models import Task
 
-# Create your views here.
+def index(req):
+    tasks = Task.objects.all()
+    return render(req, 'mainapp/index.html', {'title': 'main page of site', 'tasks': tasks})
+
+def about(req):
+    return render(req, 'mainapp/about.html')
